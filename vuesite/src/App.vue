f05df3e35efcb801becd02c7b9dbcca2a756a349
Taylor Daly <taylor.daly@ucdenver.edu>
2020-11-18 20:57:02 -0700

    Starting to build clickable in vue

--- vuesite/src/App.vue
+++ vuesite/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Clickable/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clickable from './components/Clickable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Clickable
   }
 }
 </script>
--- vuesite/src/main.js
+++ vuesite/src/main.js
@@ -6,3 +6,15 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+  el: '#Clickable',
+  data: {
+    counter: 0
+  },
+  methods: {
+    click: function () {
+      this.counter+=1
+    }
+  }
+})
