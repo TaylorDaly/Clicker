9585a7e025ff17307281532821a8d31d4dd2ba15
John Fuller <49049456+FullJohn@users.noreply.github.com>
2020-12-30 17:53:21 -0700

    Added basic chatbox framework to djangosite

--- djangosite/djangosite/readme.md
+++ djangosite/djangosite/readme.md
@@ -1,12 +1,13 @@
 ### Installation  
-> 1. create virtual python environment
+> 1. Create virtual python environment
 > 1. `pip install Django`
 > 1. `pip install channels`
 >       - If working on windows, before installing channels download twisted from  
         https://www.lfd.uci.edu/~gohlke/pythonlibs/  
         and use pip install Twisted‑20.3.0‑cp39‑cp39‑win_amd64.whl  
         (Twisted gave me trouble when working in windows, this version is pre-compiled)  
-
+> 1. Install Docker for desktop on Windows
+>       - https://docs.docker.com/docker-for-windows/install/
 ---
   
 ### Running  
--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels'
+    'channels',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +117,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# mysite/settings.py
+# Channels
+ASGI_APPLICATION = 'djangosite.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
